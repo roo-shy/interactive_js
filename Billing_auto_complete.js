fd1a2472cab29542deb5543bc564779b654e2b00
at-rooshee <ru_sha_nia@yahoo.ca>
2016-11-13 23:51:40 -0600

    Adding billing auto complete

---
+++ Billing_auto_complete.js
@@ -0,0 +1,15 @@
+   function billingFunction() {
+      var shipName = document.getElementById('shippingName');
+      var shipZip = document.getElementById('shippingZip');
+      var billName = document.getElementById('billingName');
+      var billZip =document.getElementById('billingZip');
+      var checkBox = document.getElementById('same');
+       if (checkBox.checked) {
+        billName.value = shipName.value;
+        billZip.value = shipZip.value;
+       }
+       else {
+        billName.value = null;
+        billZip.value = null;
+       }
+    }
