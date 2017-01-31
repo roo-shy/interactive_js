372200aafb85e7c6f6f019c9c932fff3a4d326eb
at-rooshee <ru_sha_nia@yahoo.ca>
2017-01-30 23:56:39 -0600

    Renaming the files

---
+++ landscape_function.js
@@ -0,0 +1,25 @@
+// Drawing with JavaScript need to comment each line and make a new landscape
+    
+  var landscape = function() {
+  var result = "";
+  var flat = function(size) {
+    for (var count = 0; count < size; count++)
+      result += "_";
+  };
+  var mountain = function(size) {
+    result += "/";
+    for (var count = 0; count < size; count++)
+      result += "'";
+    result += "\\";
+  };
+
+  flat(3);
+  mountain(4);
+  flat(6);
+  mountain(1);
+  flat(1);
+  return result;
+};
+
+console.log(landscape());
+// → ___/''''\______/'\_
